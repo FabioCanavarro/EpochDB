7ec07407b7e829907d36711ce81c3fca47bb81c6
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-08-05 21:17:29 +0700

    Succesfully created a basic app at localhost:3000

---
+++ src/bin/epoch-metric.rs
@@ -0,0 +1,16 @@
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/metrics", get(async || {"HELLOOO"})); 
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+
+async fn get_metric() {}
