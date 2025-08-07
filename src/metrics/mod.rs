0eab3faa024e65308c44fecfb25604b07faecc2a
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-08-07 21:16:42 +0700

    Metrics opts

--- src/metrics/mod.rs
+++ src/metrics/mod.rs
@@ -12,5 +12,44 @@ pub struct Metrics {
 
 impl Metrics {
     fn new() -> Self {
+        let keys_total_opts = Opts::new(
+            "epochdb_keys_total",
+            "Total number of keys in a tree"
+        );
+
+        let operations_total_opts = Opts::new(
+            "epochdb_operations_total",
+            "Total number of operations"
+        );
+
+        let disk_size_opts = Opts::new(
+            "epochdb_disk_size",
+            "Size of the directory"
+        );
+
+        let backup_size_opts = Opts::new(
+            "epochdb_backup_size",
+            "Size of the backup"
+        ); 
+
+        let ttl_expired_keys_total_opts = Opts::new(
+            "epochdb_ttl_expired_keys_total_opts",
+            "Total amount of expired ttl keys"
+        );
+
+        let cache_hits_opts = Opts::new(
+            "epochdb_cache_hits_total",
+            "Total amount of cache hits"
+        );
+
+        let cache_misses_total = Opts::new(
+            "epochdb_cache_misses_total",
+            "Total amount of cache misses"
+        );
+
+        let keys_total = IntGaugeVec::new(
+            keys_total_opts,
+            &["data", "meta", "ttl"]
+        );
     }
 }
