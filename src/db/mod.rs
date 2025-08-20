00dfaa3e1c58694e03cd17194d8524d9156bd3a4
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-08-20 21:36:38 +0700

    Restructured and cargo fmt

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -3,18 +3,17 @@
 //! primary API for interacting with the database.
 
 pub mod errors;
+pub mod iter;
+pub mod transaction;
 
 use crate::{DB, Metadata, metrics::Metrics};
 use chrono::Local;
 use errors::TransientError;
 use sled::{
     Config,
-    transaction::{
-        ConflictableTransactionError, TransactionError, Transactional, TransactionalTree,
-    },
+    transaction::{ConflictableTransactionError, TransactionError, Transactional},
 };
 use std::{
-    error::Error,
     fs::File,
     io::{ErrorKind, Read, Write},
     path::Path,
@@ -371,7 +370,7 @@ impl DB {
     /// - Any corresponding folder in the path is not found
     /// - Zip or sled fails because of any reason
     /// - IOError when the file is being access by the OS for something else
-    /// - Failing to parse any data to a [u8] 
+    /// - Failing to parse any data to a [u8]
     pub fn backup_to(&self, path: &Path) -> Result<(), TransientError> {
         self.flush()?;
 
@@ -453,7 +452,7 @@ impl DB {
     }
 
     // WARN: Add a transactional batching algorithm to ensure safety incase of a power outage
-    
+
     /// This function loads the backup archive from the path given and loads the database in the
     /// db_path
     ///
@@ -554,47 +553,6 @@ impl DB {
 
         Ok(db)
     }
-
-    /// This function returns the iterator of the database, which will contain a key and its
-    /// corresponding value in each iteration, (key, value).
-    pub fn iter(&mut self) -> DataIter {
-        DataIter {
-            data: (self.data_tree.iter(), self.meta_tree.clone()),
-        }
-    }
-
-    pub fn transaction<F>(&self, f: F) -> Result<(), TransientError>
-    where
-        F: Fn(&mut TransactionalGuard) -> Result<(), Box<dyn Error>>,
-    {
-        let l: Result<GuardMetricChanged, TransactionError<()>> =
-            (&*self.data_tree, &*self.meta_tree, &*self.ttl_tree).transaction(
-                |(data_tree, meta_tree, ttl_tree)| {
-                    let mut guard_metrics = GuardMetricChanged {
-                        keys_total_changed: 0,
-                        ttl_keys_total_changed: 0,
-                        set_operation_total: 0,
-                        rm_operation_total: 0,
-                        inc_freq_operation_total: 0,
-                        get_operation_total: 0,
-                    };
-                    let mut transaction_guard = TransactionalGuard {
-                        data_tree,
-                        meta_tree,
-                        ttl_tree,
-                        changed_metric: &mut guard_metrics,
-                    };
-                    f(&mut transaction_guard)
-                        .map_err(|_| ConflictableTransactionError::Abort(()))?;
-
-                    Ok(guard_metrics)
-                },
-            );
-
-        l.map_err(|_| TransientError::SledTransactionError)?
-            .inc_all_metrics();
-        Ok(())
-    }
 }
 
 impl Drop for DB {
@@ -617,260 +575,5 @@ impl Drop for DB {
             .expect("Fail to take ownership of ttl_thread")
             .join()
             .expect("Joining failed");
-    }
-}
-
-/// This is an iterator struct that represents the Database main iterator struct.
-pub struct DataIter {
-    pub data: (sled::Iter, Arc<sled::Tree>),
-}
-
-impl Iterator for DataIter {
-    type Item = Result<(String, String, Metadata), Box<dyn Error>>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let data_iter = &mut self.data.0;
-
-        let data = match data_iter.next()? {
-            Ok(a) => a,
-            Err(e) => {
-                return Some(Err(Box::new(e)));
-            }
-        };
-
-        let (kb, vb) = data;
-
-        let meta_tree = &mut self.data.1;
-
-        let mb = match meta_tree.get(&kb) {
-            Ok(a) => a,
-            Err(e) => {
-                return Some(Err(Box::new(e)));
-            }
-        }?;
-
-        let key = match from_utf8(&kb) {
-            Ok(a) => a,
-            Err(e) => {
-                return Some(Err(Box::new(e)));
-            }
-        }
-        .to_string();
-
-        let value = match from_utf8(&vb) {
-            Ok(a) => a,
-            Err(e) => {
-                return Some(Err(Box::new(e)));
-            }
-        }
-        .to_string();
-
-        let meta = match Metadata::from_u8(&mb) {
-            Ok(a) => a,
-            Err(e) => {
-                return Some(Err(Box::new(e)));
-            }
-        };
-
-        Some(Ok((key, value, meta)))
-    }
-}
-
-struct GuardMetricChanged {
-    keys_total_changed: i64,
-    ttl_keys_total_changed: i64,
-    set_operation_total: u64,
-    rm_operation_total: u64,
-    inc_freq_operation_total: u64,
-    get_operation_total: u64,
-}
-
-impl GuardMetricChanged {
-    fn inc_all_metrics(&self) {
-        let i = self.keys_total_changed;
-
-        if i > 0 {
-            Metrics::inc_amount_keys_total("data", i.unsigned_abs());
-            Metrics::inc_amount_keys_total("meta", i.unsigned_abs());
-        } else {
-            Metrics::dec_amount_keys_total("data", i.unsigned_abs());
-            Metrics::dec_amount_keys_total("meta", i.unsigned_abs());
-        }
-
-        let i = self.ttl_keys_total_changed;
-
-        if i > 0 {
-            Metrics::inc_amount_keys_total("ttl", i.unsigned_abs());
-        } else {
-            Metrics::dec_amount_keys_total("ttl", i.unsigned_abs());
-        }
-
-        Metrics::increment_amount_operations("set", self.set_operation_total);
-        Metrics::increment_amount_operations("rm", self.rm_operation_total);
-        Metrics::increment_amount_operations("increment_frequency", self.inc_freq_operation_total);
-        Metrics::increment_amount_operations("get", self.get_operation_total);
-    }
-}
-
-/// This struct is the Guard for the database Transaction method.
-///
-/// This struct holds all the trees of the main database and the changed_metric, It provides us
-/// with the ability to make transactions for better data safety.
-///
-/// When the transaction method concludes this struct, will check all the changed_metric, and
-/// will increment or decrement the corresponding metric in the real database, to ensure that the
-/// correct metrics will be shown.
-pub struct TransactionalGuard<'a> {
-    data_tree: &'a TransactionalTree,
-    meta_tree: &'a TransactionalTree,
-    ttl_tree: &'a TransactionalTree,
-    changed_metric: &'a mut GuardMetricChanged,
-}
-
-// NOTE: The reason why I didn't convert everything to Transient error is because of the
-// UnabortableTransactionError enum, where is error, they will reset,
-// If I fuck with this who knows what will be fucked up TT
-impl<'a> TransactionalGuard<'a> {
-    /// Sets a key-value pair with an optional Time-To-Live (TTL).
-    ///
-    /// If the key already exists, its value and TTL will be updated.
-    /// If `ttl` is `None`, the key will be persistent.
-    ///
-    /// # Errors
-    ///
-    /// This function can return an error if there's an issue with the underlying
-    pub fn set(
-        &mut self,
-        key: &str,
-        val: &str,
-        ttl: Option<Duration>,
-    ) -> Result<(), Box<dyn Error>> {
-        let data_tree = &self.data_tree;
-        let freq_tree = &self.meta_tree;
-        let ttl_tree = &self.ttl_tree;
-        let byte = key.as_bytes();
-        let ttl_sec = match ttl {
-            Some(t) => {
-                let systime = SystemTime::now()
-                    .duration_since(UNIX_EPOCH)
-                    .expect("Cant get SystemTime");
-                Some((t + systime).as_secs())
-            }
-            None => None,
-        };
-
-        match freq_tree.get(byte)? {
-            Some(m) => {
-                let mut meta = Metadata::from_u8(&m)?;
-                if let Some(t) = meta.ttl {
-                    let _ = ttl_tree.remove([&t.to_be_bytes()[..], byte].concat());
-                }
-                meta.ttl = ttl_sec;
-                freq_tree.insert(byte, meta.to_u8()?)?;
-            }
-            None => {
-                freq_tree.insert(byte, Metadata::new(ttl_sec).to_u8()?)?;
-            }
-        }
-
-        data_tree.insert(byte, val.as_bytes())?;
-
-        if let Some(d) = ttl_sec {
-            ttl_tree.insert([&d.to_be_bytes()[..], byte].concat(), byte)?;
-            self.changed_metric.ttl_keys_total_changed += 1;
-        };
-
-        // Prometheus metrics
-        self.changed_metric.keys_total_changed += 1;
-        self.changed_metric.set_operation_total += 1;
-
-        Ok(())
-    }
-
-    /// Retrieves the value for a given key.
-    ///
-    /// # Errors
-    ///
-    /// Returns an error if the value cannot be retrieved from the database or if
-    /// the value is not valid UTF-8.
-    pub fn get(&mut self, key: &str) -> Result<Option<String>, Box<dyn Error>> {
-        let data_tree = &self.data_tree;
-        let byte = key.as_bytes();
-        let val = data_tree.get(byte)?;
-
-        self.changed_metric.get_operation_total += 1;
-
-        match val {
-            Some(val) => Ok(Some(from_utf8(&val)?.to_string())),
-            None => Ok(None),
-        }
-    }
-
-    /// Atomically increments the frequency counter for a given key.
-    ///
-    /// # Errors
-    ///
-    /// This function can return an error if the key does not exist or if there
-    /// is an issue with the compare-and-swap operation.
-    pub fn increment_frequency(&mut self, key: &str) -> Result<(), Box<dyn Error>> {
-        let freq_tree = &self.meta_tree;
-        let byte = &key.as_bytes();
-
-        let metadata = freq_tree
-            .get(byte)?
-            .ok_or(TransientError::IncretmentError)?;
-        let meta = Metadata::from_u8(&metadata)?;
-
-        freq_tree.remove(*byte)?;
-        freq_tree.insert(*byte, meta.freq_incretement().to_u8()?)?;
-
-        self.changed_metric.inc_freq_operation_total += 1;
-
-        Ok(())
-    }
-
-    /// Removes a key-value pair and its associated metadata from the database.
-    ///
-    /// # Errors
-    ///
-    /// Can return an error if the transaction to remove the data fails.
-    pub fn remove(&mut self, key: &str) -> Result<(), Box<dyn Error>> {
-        let data_tree = &self.data_tree;
-        let freq_tree = &self.meta_tree;
-        let ttl_tree = &self.ttl_tree;
-        let byte = &key.as_bytes();
-        data_tree.remove(*byte)?;
-        let meta = freq_tree
-            .get(byte)?
-            .ok_or(TransientError::MetadataNotFound)?;
-        let time = Metadata::from_u8(&meta)?.ttl;
-        freq_tree.remove(*byte)?;
-
-        self.changed_metric.keys_total_changed -= 1;
-
-        if let Some(t) = time {
-            self.changed_metric.ttl_keys_total_changed -= 1;
-
-            let _ = ttl_tree.remove([&t.to_be_bytes()[..], &byte[..]].concat());
-        }
-
-        self.changed_metric.rm_operation_total += 1;
-
-        Ok(())
-    }
-
-    /// Retrieves the metadata for a given key.
-    ///
-    /// # Errors
-    ///
-    /// Returns an error if the metadata cannot be retrieved or deserialized.
-    pub fn get_metadata(&self, key: &str) -> Result<Option<Metadata>, Box<dyn Error>> {
-        let freq_tree = &self.meta_tree;
-        let byte = key.as_bytes();
-        let meta = freq_tree.get(byte)?;
-        match meta {
-            Some(val) => Ok(Some(Metadata::from_u8(&val)?)),
-            None => Ok(None),
-        }
     }
 }
