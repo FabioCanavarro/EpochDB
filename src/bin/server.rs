aa6d97dceb49556262943af9a0e6da25ebc58c28
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-08-31 15:51:49 +0700

    Added initial struct and enum and changed the fn

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,22 @@
 use std::{error::Error, net::{TcpListener, TcpStream}};
 
+use epoch_db::DB;
 use tokio::spawn;
+
+struct ParsedResponse<'a> {
+    command: String,
+    key: Option<&'a str>,
+    value: Option<&'a str>,
+    ttl: Option<u64>
+}
+
+enum Command {
+    Set,
+    Get,
+    Rm,
+    IncrementFrequency,
+    GetMetadata
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -14,10 +30,10 @@ async fn parse_byte(byte: &[u8]) {
     todo!()
 }
 
-async fn execute_commands(command: todo!()) {
+async fn execute_commands(db: DB, command: Command) {
     todo!()
 }
 
-async fn stream_feedback(stream: TcpStream, feedback: todo!()) {
+async fn stream_feedback(stream: TcpStream, feedback: Result<&[u8], Box<dyn Error>>) {
     todo!()
 }
