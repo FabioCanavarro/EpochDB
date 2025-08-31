31b6a8bcafacb3a3c63654d8d81ead47b1b2a5c9
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-08-31 16:00:18 +0700

    Intial function and a few notes for possible repl

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, net::{TcpListener, TcpStream}};
 
-use epoch_db::DB;
+use epoch_db::{db::errors::TransientError, DB};
 use tokio::spawn;
 
 struct ParsedResponse<'a> {
@@ -20,20 +20,35 @@ enum Command {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = "localhost:3001";
+    let listener = TcpListener::bind(addr)?;
+    loop {
+        let stream = listener.accept()?.0;
+        let handler = spawn(response_handler(stream));
+    }
 }
 
-async fn response_handler(stream: TcpStream) {
+async fn response_handler(stream: TcpStream) -> Result<(), TransientError> {
     todo!()
 }
 
-async fn parse_byte(byte: &[u8]) {
+async fn parse_byte(byte: &[u8]) -> Result<Command, Box<dyn Error>> {
     todo!()
 }
 
-async fn execute_commands(db: DB, command: Command) {
+async fn execute_commands(db: DB, command: Command) -> Result<Option<Vec<u8>>, Box<dyn Error>> {
     todo!()
 }
 
-async fn stream_feedback(stream: TcpStream, feedback: Result<&[u8], Box<dyn Error>>) {
+async fn stream_feedback(stream: TcpStream, feedback: Result<&[u8], Box<dyn Error>>) -> Result<(), Box<dyn Error>> {
     todo!()
 }
+
+/* TODO:
+*   Add a REPL during the loop, so they can issue commands such as:
+*   - Backup, Load(seperated to 2?, one for overwrite one just adds non existing keys)
+*   - Check amount of tll keys
+*   - Check highest freq
+*   - Check amount of ttl keys
+*   - Be able to Get, Set, rm directly from server for speed
+*/
