c86f03db5d910933c980137634d1884a130fff69
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-08-31 15:45:46 +0700

    initial functions

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,23 @@
+use std::{error::Error, net::{TcpListener, TcpStream}};
+
+use tokio::spawn;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
+}
+
+async fn response_handler(stream: TcpStream) {
+    todo!()
+}
+
+async fn parse_byte(byte: &[u8]) {
+    todo!()
+}
+
+async fn execute_commands(command: todo!()) {
+    todo!()
+}
+
+async fn stream_feedback(stream: TcpStream, feedback: todo!()) {
+    todo!()
 }
