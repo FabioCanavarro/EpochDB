05ceada195c88799f9514c07c2957d889e194c4f
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-10-30 21:15:08 +0700

    Added all valid test cases
    Holy shit, This is torturous but fun at the same time; and i have more tests cases :( / :)

--- tests/server_parser_test.rs
+++ tests/server_parser_test.rs
@@ -6,7 +6,6 @@ use epoch_db::server::commands::{
     ParsedResponse
 };
 use epoch_db::server::parse_command;
-use futures::future;
 use tokio::io::BufReader;
 
 async fn parse_test_command(input: &[u8]) -> ParsedResponse {
@@ -66,3 +65,153 @@ async fn test_parse_set_empty_value() {
         }
     )
 }
+
+/* pastable
+#[tokio::test]
+async fn name() {
+    todo!();
+}
+*/
+
+#[tokio::test]
+async fn test_parse_set_binary_value() {
+    let input = b"*3\r\n$3\r\nSET\r\n$4\r\n\xDE\xAD\xBE\xEF\r\n$2\r\n\xCA\xFE\r\n";
+    let r = parse_test_command(input).await;
+
+    assert_eq!(
+        r,
+        ParsedResponse {
+            command: Command::Set,
+            key: Some(vec![0xDE, 0xAD, 0xBE, 0xEF]),
+            value: Some(vec![0xCA, 0xFE]),
+            ttl: None,
+            len: 3,
+        }
+    )
+}
+
+#[tokio::test]
+async fn test_parse_remove_simple() {
+    let input = b"*2\r\n$2\r\nRM\r\n$3\r\nkey\r\n";
+    let r = parse_test_command(input).await;
+
+    assert_eq!(
+        r,
+        ParsedResponse {
+            command: Command::Rm,
+            key: Some(b"key".to_vec()),
+            value: None,
+            ttl: None,
+            len: 2,
+        }
+    )
+}
+
+#[tokio::test]
+async fn test_parse_increment_frequency_simple() {
+    let input = b"*2\r\n$19\r\nINCREMENT_FREQUENCY\r\n$8\r\ncounter1\r\n";
+    let r = parse_test_command(input).await;
+
+    assert_eq!(
+        r,
+        ParsedResponse {
+            command: Command::IncrementFrequency,
+            key: Some(b"counter1".to_vec()),
+            value: None,
+            ttl: None,
+            len: 2,
+        }
+    )
+}
+
+#[tokio::test]
+async fn test_parse_get_metadata_simple() {
+    let input = b"*2\r\n$12\r\nGET_METADATA\r\n$3\r\nkey\r\n";
+    let r = parse_test_command(input).await;
+
+    assert_eq!(
+        r,
+        ParsedResponse {
+            command: Command::GetMetadata,
+            key: Some(b"key".to_vec()),
+            value: None,
+            ttl: None,
+            len: 2,
+        }
+    )
+}
+
+#[tokio::test]
+async fn test_parse_size() {
+    let input = b"*1\r\n$4\r\nSIZE\r\n";
+    let r = parse_test_command(input).await;
+
+    assert_eq!(
+        r,
+        ParsedResponse {
+            command: Command::Size,
+            key: None,
+            value: None,
+            ttl: None,
+            len: 1,
+        }
+    )
+}
+
+#[tokio::test]
+async fn test_parse_ping() {
+    let input = b"*1\r\n$4\r\nPING\r\n";
+    let r = parse_test_command(input).await;
+
+    assert_eq!(
+        r,
+        ParsedResponse {
+            command: Command::Ping,
+            key: None,
+            value: None,
+            ttl: None,
+            len: 1,
+        }
+    )
+}
+
+#[tokio::test]
+async fn test_parse_flush() {
+    let input = b"*1\r\n$5\r\nFLUSH\r\n";
+    let r = parse_test_command(input).await;
+
+    assert_eq!(
+        r,
+        ParsedResponse {
+            command: Command::Flush,
+            key: None,
+            value: None,
+            ttl: None,
+            len: 1,
+        }
+    )
+}
+
+#[tokio::test]
+async fn test_parse_command_case_sensitivity() {
+    let input = b"*2\r\n$3\r\nget\r\n$3\r\nkey\r\n";
+    let r = parse_test_command(input).await;
+
+    assert_eq!(
+        r,
+        ParsedResponse {
+            command: Command::Get,
+            key: Some(b"key".to_vec()),
+            value: None,
+            ttl: None,
+            len: 2,
+        }
+    )
+}
+
+
+
+
+
+
+
