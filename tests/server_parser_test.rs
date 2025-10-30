eed32322e6ac498f0be695b9ef1e9760f81f29f2
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-10-30 20:45:42 +0700

    Made the test more modular and simpler for better error handling; Added a new parse_test_command more suited for tests

--- tests/server_parser_test.rs
+++ tests/server_parser_test.rs
@@ -9,28 +9,19 @@ use epoch_db::server::parse_command;
 use futures::future;
 use tokio::io::BufReader;
 
-#[tokio::test]
-async fn valid_command_test() {
-    /*
-     * *2\r\n$3\r\nGET\r\n$3\r\nkey\r\n
-     * *4\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$4\r\n1000\r\n
-     * *3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$0\r\n\r\n
-     */
-    let buf_arr: &[&[u8]] = &[
-        b"*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n",
-        b"*4\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$4\r\n1000\r\n".as_ref(),
-        b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$0\r\n\r\n"
-    ];
+async fn parse_test_command(input: &[u8]) -> ParsedResponse {
+    let c = Cursor::new(input);
+    let mut buf_reader = BufReader::new(c);
+    parse_command(&mut buf_reader).await.unwrap()
+}
 
-    let res = buf_arr.iter().map(async |buf| {
-        let c = Cursor::new(buf);
-        let mut buf_reader = BufReader::new(c);
-        parse_command(&mut buf_reader).await.unwrap()
-    });
-    let r = future::join_all(res).await;
+#[tokio::test]
+async fn test_parse_get_simple() {
+    let input = b"*2\r\n$3\r\nGET\r\n$3\r\nkey\r\n";
+    let r = parse_test_command(input).await;
 
     assert_eq!(
-        r[0],
+        r,
         ParsedResponse {
             command: Command::Get,
             key: Some(Vec::from(b"key")),
@@ -39,24 +30,37 @@ async fn valid_command_test() {
             len: 2
         }
     );
+}
+
+#[tokio::test]
+async fn test_parse_set_with_ttl() {
+    let input = b"*4\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n$4\r\n1000\r\n";
+    let r = parse_test_command(input).await;
 
     assert_eq!(
-        r[1],
+        r,
         ParsedResponse {
             command: Command::Set,
             key: Some(Vec::from(b"key")),
             value: Some(b"value".to_vec()),
-            ttl: Some(Duration::new(1, 0)),
+            // Changed to from_millis to be explicit, since your parser uses it
+            ttl: Some(Duration::from_millis(1000)),
             len: 4
         }
     );
+}
+
+#[tokio::test]
+async fn test_parse_set_empty_value() {
+    let input = b"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$0\r\n\r\n";
+    let r = parse_test_command(input).await;
 
     assert_eq!(
-        r[2],
+        r,
         ParsedResponse {
             command: Command::Set,
             key: Some(b"key".to_vec()),
-            value: Some([].to_vec()),
+            value: Some(vec![]), // More idiomatic than [].to_vec()
             ttl: None,
             len: 3
         }
