891e183ba41a80dd1af9543f90464db67ec10174
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-11-29 23:13:20 +0700

    Cli Struct

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,5 +1,16 @@
 use clap::{Parser, Subcommand};
-use serde::Serialize;
+
+// Cli Parser
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    #[arg(short, long)]
+    addr: String,
+}
+
 fn main() {
     todo!();
 }
