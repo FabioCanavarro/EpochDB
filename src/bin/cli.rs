04a9eb4fc681fe036d8c7ce77ee46d5dacf0ae18
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-11-29 23:13:32 +0700

    Commands enum for clap

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -11,6 +11,33 @@ struct Cli {
     addr: String,
 }
 
+#[derive(Subcommand)]
+enum Commands {
+    /// Set a key-value pair
+    Set { key: String, val: String, ttl: u64 },
+
+    /// Get the value for a key
+    Get { key: String },
+
+    /// Remove a key-value pair
+    Rm { key: String },
+
+    /// Increase the frequency of a key
+    IncrementFrequency { key: String },
+
+    /// Get the metadata for a key
+    GetMetadata { key: String },
+
+    /// Ping a IP address
+    Ping,
+
+    /// Get the size of the database
+    Size,
+
+    /// Flush the database
+    Flush
+}
+
 fn main() {
     todo!();
 }
